50036198854e686477db6212f33955bf0cd3f944
누피 <for.pharm01@gmail.com>
2024-07-24 12:21:00 +0900

    스타일 수정 중

--- test-react-app/src/App.jsx
+++ test-react-app/src/App.jsx
@@ -1,26 +1,49 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import PhoneImage from './assets/Phone_rm_bg.png';
+import React from 'react';
+
+import Nav from './components/Nav';
+import FirstSection from './components/FirstSection';
+import SecondSection from './components/SecondSection';
+import ThirdSection from './components/ThirdSection';
+import FourthSection from './components/FourthSection';
+import FifthSection from './components/FifthSection';
+import SixthSection from './components/SixthSection';
+import SeventhSection from './components/SeventhSection';
+import EighthSection from './components/EighthSection';
+import Footer from './components/Footer';
+
+import styled, { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <img src={PhoneImage} />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+  <ThemeProvider theme={theme}>
+    <AppContainer>
+      <Nav />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+      <SixthSection />
+      <SeventhSection />
+      <EighthSection />
+      <Footer />
+    </AppContainer>
+  </ThemeProvider>
   );
 }
 
 export default App;
+//아래와 같은 방식으로 폰트와 컬러 사용하시면 됩니다!
+const H1 = styled.h1`
+  ${({ theme }) => theme.fonts.sansita_one};
+  color: ${({ theme }) => theme.colors.pc1};
+`;
+
+const AppContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #ffffff;
+  position: relative;
+`;
